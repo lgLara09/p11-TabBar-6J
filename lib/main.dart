10cec56435e1135f024c15c8abe8293ab2005067
LIZBETH_GARCIA_LARA <a.21308051280483@cbtis128.edu.mx>
2024-02-29 17:01:54 +0000

    Actualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,86 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppTabBar extends StatelessWidget {
+  const AppTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo TabBar",
       theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MiPaginaInicial(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+}
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("TabBar Lizbeth Garcia"),
+            bottom: TabBar(tabs: [
+              Tab(
+                text: "Libro1",
+                icon: Icon(Icons.add_comment),
+              ),
+              Tab(
+                text: "Libro2",
+                icon: Icon(Icons.threesixty_outlined),
+              ),
+              Tab(
+                text: "Libro3",
+                icon: Icon(Icons.account_box),
+              ),
+              Tab(
+                text: "Libros4",
+                icon: Icon(Icons.abc),
+              ),
+            ] //texto icono),
+                ),
+          ),
+          body: TabBarView(
+            children: const <Widget>[
+              Center(
+                child: Text(
+                  "Romance",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "Misterio",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "Terror",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "Aventura",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+              ),
+            ],
+          ),
+        ));
+  } //widgets
 }
